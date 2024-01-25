ecfc219fd5fb4fb5b83e0924f703437c6510478a
AIXDL <281180295@qq.com>
2024-01-25 14:40:53 +0800

    完整的前端项目

--- static/config/index.js
+++ static/config/index.js
@@ -5,7 +5,6 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  // window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/renren-fast';
   window.SITE_CONFIG['baseUrl'] = 'http://localhost:88/api';
 
   // cdn地址 = 域名 + 版本号
